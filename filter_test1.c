8377494ed3e5696ad0fff8f927d3e52d6222df9b
Karl Hiramoto <karl@hiramoto.org>
2010-03-09 21:35:26 +0100

    test program

--- filter_test1.c
+++ filter_test1.c
@@ -1,8 +1,16 @@
 #include "HttpReq.h"
 #include "IpFilter.h"
+#include "ContentFilter.h"
 
 int main(int argc, char *argv[])
 {
+	struct ContentFilter *cf;
+	int ret;
 
+	cf = ContentFilter_new();
+
+	ret = ContentFilter_loadConfig(cf, "Xml file");
+	
+	ContentFilter_put(&cf);
 	return 0;
 }
