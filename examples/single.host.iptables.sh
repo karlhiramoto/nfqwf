5974ed1db3df0dd01b5aa94c50053844745ffae2
Karl Hiramoto <karl@hiramoto.org>
2011-04-09 13:55:51 +0200

    example config

--- examples/single.host.iptables.sh
+++ examples/single.host.iptables.sh
@@ -14,6 +14,6 @@ $IPT -X
 
 echo -n 1 > /proc/sys/net/ipv4/ip_forward
 
-$IPT -t mangle -A INPUT -i eth0 -p tcp --sport 80 -j NFQUEUE --queue-num 1
-$IPT -t mangle -A OUTPUT  -o eth0 -p tcp  --dport 80 -j NFQUEUE --queue-num 1
-$IPT -t mangle -A FORWARD  -p tcp -m multiport --ports 80 -j NFQUEUE --queue-num 1
+$IPT -t mangle -A INPUT -i eth0 -p tcp --sport 80 -j NFQUEUE --queue-balance 1:10
+$IPT -t mangle -A OUTPUT  -o eth0 -p tcp  --dport 80 -j NFQUEUE --queue-balance 1:10
+$IPT -t mangle -A FORWARD  -p tcp -m multiport --ports 80 -j NFQUEUE --queue-balance 1:10
